44dc931d137e51334927a5be448365e7810b89ba
Thomas Huijer <thomas@nforza.nl>
2016-01-11 16:18:37 +0100

    Moved IfSQ Level 2 analyzers in its own class Introduced class to generate the report

---
+++ Sources/Inspector/Components/IfSQLevel2Analyzer.cs
@@ -0,0 +1,45 @@
+﻿namespace Inspector.Components
+{
+    class IfSQLevel2Analyzer : SourceFileAnalyzer
+    {
+        public IfSQLevel2Analyzer()
+        {
+            AddIfSQLevel2Analyzers();
+        }
+
+        private void AddIfSQLevel2Analyzers()
+        {
+            //SP-1
+            AddAnalyzer(new CodeMetrics.CSharp.MethodLength());
+            AddAnalyzer(new CodeMetrics.VisualBasic.MethodLength());
+
+            //SP-2
+            AddAnalyzer(new CodeMetrics.CSharp.NestingLevel());
+            AddAnalyzer(new CodeMetrics.VisualBasic.NestingLevel());
+
+            //SP-3
+            AddAnalyzer(new CodeMetrics.CSharp.ControlFlowComplexity());
+            AddAnalyzer(new CodeMetrics.VisualBasic.ControlFlowComplexity());
+
+            //WIP-1
+            AddAnalyzer(new CodeMetrics.CSharp.VagueToDo());
+            AddAnalyzer(new CodeMetrics.VisualBasic.VagueToDo());
+
+            //WIP-2
+            AddAnalyzer(new CodeMetrics.CSharp.DisabledCode());
+            AddAnalyzer(new CodeMetrics.VisualBasic.DisabledCode());
+
+            //WIP-3
+            AddAnalyzer(new CodeMetrics.CSharp.EmptyStatementBlock());
+            AddAnalyzer(new CodeMetrics.VisualBasic.EmptyStatementBlock());
+
+            //SPM-1
+            AddAnalyzer(new CodeMetrics.CSharp.MagicNumber());
+            AddAnalyzer(new CodeMetrics.VisualBasic.MagicNumber());
+
+            //SPM-2
+            AddAnalyzer(new CodeMetrics.CSharp.MagicString());
+            AddAnalyzer(new CodeMetrics.VisualBasic.MagicString());
+        }
+    }
+}
---
+++ Sources/Inspector/Reports/IfSQLevel2Report.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Inspector.IfSQ;
+
+namespace Inspector.Reports
+{
+    class IfSQLevel2Report
+    {
+        public void PrintMetrics(IEnumerable<Level2Score> ifsqScores)
+        {
+            PrintDetails(ifsqScores);
+            PrintTotal(ifsqScores);
+        }
+
+        private static void PrintDetails(IEnumerable<Level2Score> ifsqScores)
+        {
+            Console.WriteLine(Level2Score.HeaderString());
+            ifsqScores.OrderByDescending(s => s.Rating).ToList().ForEach(s => Console.WriteLine(s));
+        }
+
+        private static void PrintTotal(IEnumerable<Level2Score> ifsqScores)
+        {
+            int totalLines = ifsqScores.Sum(sf => sf.Loc);
+            var total = new Level2Score(totalLines, ifsqScores);
+            Console.WriteLine(total);
+        }
+
+    }
+}
